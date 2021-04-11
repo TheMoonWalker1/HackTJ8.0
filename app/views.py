d5f921a2a626d38693c3713c4365aeb21333ecdd
Praneeth Chinmay Bhandaru <praneethsbhandaru@gmail.com>
2021-04-11 10:16:28 -0400

    Added Random VIN generator for the home page

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from .forms import *
 from .models import *
 from urllib.request import urlopen, Request
 import json
+import random
 
 
 # Create your views here.
@@ -35,7 +36,6 @@ def signup(request):
 
 
 def home(request):
-
     if request.method == "POST":
         form = CarForm(request.POST)
         if form.is_valid():
@@ -46,4 +46,6 @@ def home(request):
             response_body = {d['Variable']: d['Value'].upper() for d in json.loads(urlopen(thing).read())['Results'] if d['Value'] != '0' and d['Value'] != 0 and d['Value'] is not None and d['Value'] != '' and d['Value'] != 'Not Applicable' and d['Variable'] != 'Error Text'}
             return render(request, 'results.html', {'info': response_body})
         messages.error(request, 'This VIN is invalid. It must be 17 characters long.')
-    return render(request, 'home.html', {'form': CarForm()})
+
+    vins = ['3GNEC12078G276688', '5GRGN23U83H132227', '1C3CDFBB5FD165342', '1FMZU62E12ZC12617', '2FAFP71W93X138259', '1FTPW12V17FA12080', '1FT8W3BT4FEA96950', 'WDXPD944555842209', 'WVGBV7AX6CW559712', 'SCFAC23302B500083', 'JH4KA7630NC013822', 'JH4DA175XGS009825', '1GCEK14K8RE106083', '4F2CU08102KM50866', 'JH4DA1850HS006058', '5N1AN0NU6BC506916', '1FVACYDC37HW59012']
+    return render(request, 'home.html', {'form': CarForm(), 'vin': random.choice(vins)})
